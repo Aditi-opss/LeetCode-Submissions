009d1ead9476ef1d0c1e838b1b1b68dc125aedf9
Aditi-opss <maheshwariaditi2609@gmail.com>
2024-08-15 22:44:07 +0530

    Time: 2 ms (82.15%) | Memory: 44.8 MB (54.57%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -3,7 +3,6 @@ class Solution {
     public int maxProduct(int[] nums) {
         int mn = nums[0], mx = nums[0], ans = nums[0];
         for(int i = 1; i < nums.length; i++) {
-            ans = Math.max(ans, mx);
             if(nums[i] < 0) {
                 int temp = mn;
                 mn = mx;
@@ -11,8 +10,8 @@ class Solution {
             }
             mx = Math.max(nums[i], mx * nums[i]);
             mn = Math.min(nums[i],nums[i] * mn);
+            ans = Math.max(ans, mx);
         }
-        ans = Math.max(ans, mx);
         return ans;
     }
 }
