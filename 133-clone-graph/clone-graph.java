f60f05c3755222a6e406f799fd66e067e91dd9f8
Aditi-opss <maheshwariaditi2609@gmail.com>
2024-08-24 13:43:58 +0530

    Time: 26 ms (61.51%) | Memory: 43.3 MB (7.83%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node newNode  = new Node(node.val);
+        map.put(node, newNode);
+        deepCopy(node, map);
+
+        return newNode;
+    }
+
+    public void deepCopy(Node oldNode, Map<Node, Node> map) {
+        if(oldNode == null) return;
+        
+        List<Node> newNeighList = new ArrayList<>();
+        for(Node oldNeigh : oldNode.neighbors) {
+            if(map.containsKey(oldNeigh)) newNeighList.add(map.get(oldNeigh));
+            else {
+                Node newNeigh = new Node(oldNeigh.val);
+                map.put(oldNeigh, newNeigh);
+                newNeighList.add(newNeigh);
+                deepCopy(oldNeigh, map);
+            }
+        }
+        map.get(oldNode).neighbors = newNeighList;
+    }
+}
