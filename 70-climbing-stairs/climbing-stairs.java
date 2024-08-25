61e78d0a2143c492d91952ec8ed4d5171b32adca
Aditi-opss <maheshwariaditi2609@gmail.com>
2024-08-25 21:20:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (25.55%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        Integer[] dp = new Integer[n + 1];
+        return climbStairsHelper(n, dp);
+    }
+
+    public int climbStairsHelper(int n, Integer[] dp) {
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+
+        if(dp[n] != null) 
+            return dp[n];
+
+        return dp[n] = climbStairsHelper(n-1, dp) + climbStairsHelper(n-2, dp);
+    }
+}
