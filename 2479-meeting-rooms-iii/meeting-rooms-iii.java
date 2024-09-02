f9636163402e4f6f47ded76c53088f4b723d7e74
Aditi-opss <maheshwariaditi2609@gmail.com>
2024-09-02 13:54:54 +0530

    Time: 67 ms (72.71%) | Memory: 98.8 MB (52.42%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
         Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
+        //endtime, room no.
         PriorityQueue<int[]> busy
             = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         PriorityQueue<Integer> idle = new PriorityQueue<>();
@@ -8,7 +9,7 @@ class Solution {
             idle.offer(i);
         }
         int[] cnt = new int[n];
-        for (var v : meetings) {
+        for (int[] v : meetings) {
             int s = v[0], e = v[1];
             while (!busy.isEmpty() && busy.peek()[0] <= s) {
                 idle.offer(busy.poll()[1]);
@@ -18,7 +19,7 @@ class Solution {
                 i = idle.poll();
                 busy.offer(new int[] {e, i});
             } else {
-                var x = busy.poll();
+                int[] x = busy.poll();
                 i = x[1];
                 busy.offer(new int[] {x[0] + e - s, i});
             }
